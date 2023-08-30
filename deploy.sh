0bb2c94fbd1f0db7aa509b17e6944207066d783f
Shin-ichi Ueda <skyriser@levolution.info>
2023-08-30 12:59:35 +0900

    remove peco install

--- deploy.sh
+++ deploy.sh
@@ -25,22 +25,3 @@ ln -sfnv "$PWD/vim" "$HOME/.vim"
 
 # Make local dirs
 mkdir -p $HOME/local/bin $HOME/local/etc $HOME/local/include $HOME/local/lib $HOME/local/src $HOME/local/tmp
-
-# Install peco
-case "${OSTYPE}" in
-darwin*)
-  brew install peco
-  ;;
-linux*)
-  if [ ! -e "$HOME/local/bin/peco" ]; then
-    echo "Install peco..."
-    cd $HOME/local/tmp/
-    wget https://github.com/peco/peco/releases/download/v0.3.6/peco_linux_386.tar.gz
-    cd $HOME/local/tmp/
-    tar zxvf peco_linux_386.tar.gz
-    mv peco_linux_386/peco ~/local/bin/
-    rm -rf peco_linux_386
-    rm peco_linux_386.tar.gz
-  fi
-  ;;
-esac

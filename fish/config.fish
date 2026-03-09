70fa87c570426b46e9fd89108782504963a9e83a
Shin-ichi Ueda <ueda@fact-real.com>
2026-03-09 10:54:27 +0900

    disable starship

--- fish/config.fish
+++ fish/config.fish
@@ -2,14 +2,11 @@
 fish_add_path $HOME/.local/bin $HOME/local/bin $HOME/local/sbin $HOME/bin
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
 
-# 環境変数
+# Global variables
 set -gx EDITOR vim
 set -gx LANG en_US.UTF-8
 
-# Starship
-starship init fish | source
-
-# ローカル設定
+# Local variables
 if test -f ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
 end
